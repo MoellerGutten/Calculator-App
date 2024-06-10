de7235d9566aabc2752e057e1e49f29b5735eac8
MoellerGutten <93318104+MoellerGutten@users.noreply.github.com>
2024-06-10 12:32:18 +0200

    Comments added and warnings fixed

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -6,103 +6,103 @@ class MainKtTest {
 
     @Test
     fun TestSum1() {
-        val expected: Double = 246.0
+        val expected = 246.0
         assertEquals(expected, main("123+123"))
     }
 
     @Test
     fun TestSum2() {
-        val expected: Double = 0.0
+        val expected = 0.0
         assertEquals(expected, main("123-123"))
     }
 
     @Test
     fun TestSum3() {
-        val expected: Double = -246.0
+        val expected = -246.0
         assertEquals(expected, main("-123-123"))
     }
 
     @Test
     fun TestSum4() {
-        val expected: Double = 246.0
+        val expected = 246.0
         assertEquals(expected, main("123+123+123-123"))
     }
 
     @Test
     fun TestSum5() {
-        val expected: Double = 0.0
+        val expected= 0.0
         assertEquals(expected, main("-0+0+0+0-0"))
     }
 
     @Test
     fun TestMultiplication1() {
-        val expected: Double = 25.0
+        val expected = 25.0
         assertEquals(expected, main("5*5"))
     }
 
     @Test
     fun TestMultiplication2() {
-        val expected: Double = 625.0
+        val expected = 625.0
         assertEquals(expected, main("5*5*5*5"))
     }
 
     @Test
     fun TestMultiplication3() {
-        val expected: Double = 50.0
+        val expected = 50.0
         assertEquals(expected, main("5*5+5*5"))
     }
 
     @Test
     fun TestMultiplication4() {
-        val expected: Double = 25.0
+        val expected = 25.0
         assertEquals(expected, main("5*5+5-5"))
     }
 
     @Test
     fun TestMultiplication5() {
-        val expected: Double = -25.0
+        val expected = -25.0
         assertEquals(expected, main("-5*5"))
     }
 
     @Test
     fun TestMultiplication6() {
-        val expected: Double = 155.0
+        val expected = 155.0
         assertEquals(expected, main("5+5*5*5+5*5"))
     }
 
     @Test
     fun TestMultiplication7() {
-        val expected: Double = 25.0
+        val expected = 25.0
         assertEquals(expected, main("-5*-5"))
     }
 
     @Test
     fun TestMultiplication8() {
-        val expected: Double = -25.0
+        val expected = -25.0
         assertEquals(expected, main("5*-5"))
     }
 
     @Test
     fun TestDivision1() {
-        val expected: Double = 1.0
+        val expected = 1.0
         assertEquals(expected, main("10/10"))
     }
 
     @Test
     fun TestDivision2() {
-        val expected: Double = 10.0
+        val expected = 10.0
         assertEquals(expected, main("8/2*2+2"))
     }
 
     @Test
     fun TestDivision3() {
-        val expected: Double = 75.289
+        val expected = 75.289
         assertEquals(expected, main("7.6/9*11+66"))
     }
 
     @Test
     fun TestDivision4() {
-        val expected: Double = 1.0
+        val expected = 1.0
         assertEquals(expected, main("1000/10/10/10"))
     }
 
@@ -114,7 +114,7 @@ class MainKtTest {
 
     @Test
     fun TestDivision6() {
-        val expected: Double = 1.0
+        val expected = 1.0
         assertEquals(expected, main("1/1"))
     }
 
